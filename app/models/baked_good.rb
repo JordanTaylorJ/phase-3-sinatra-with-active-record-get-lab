04d661663d82a0df1b714bb08ecbf1ca94e47216
JordanJoseph <jordant.joseph@outlook.com>
2022-07-19 15:08:10 -0600

    commit tree 6dfcfccde8c9d3b96389c04991a0ad7122f16395

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,13 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery
+
+  def self.by_price
+    self.all.order(:price).reverse_order
+  end
+
+  def self.most_expensive
+    self.all.order(:price).reverse_order.first
+  end
+
 end
